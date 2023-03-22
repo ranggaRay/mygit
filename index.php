9bca17bc681dae77ebf56dc3c9e6aa3d844753bc
unknown <Bopakray18@gmail.com>
2023-03-22 13:34:46 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+class contoh1extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Rangga Pramana Putra
+        Saya tinggal di bojonggede
+        olahraga yang saya sukai sepakbola
+        ";
+    }
+}
